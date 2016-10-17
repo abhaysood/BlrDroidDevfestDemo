d9505e496cd927e6b504a251da4556af8e7441a9
Abhay Sood <soodabhay23@gmail.com>
2016-10-17 15:41:59 +0530

    Fix a wrong package import in DaggerInjector

--- app/src/main/java/com/abhay23/blrdroiddevfestdemo/di/DaggerInjector.java
+++ app/src/main/java/com/abhay23/blrdroiddevfestdemo/di/DaggerInjector.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import com.abhay23.blrdroiddevfestdemo.ContactsApplication;
 import com.abhay23.blrdroiddevfestdemo.contacts_list.ContactsActivity;
 import com.abhay23.blrdroiddevfestdemo.di.contacts_list.ContactsModule;
-import com.abhay23.blrdroiddevfestdemo.contacts_list.DaggerContactsComponent;
+import com.abhay23.blrdroiddevfestdemo.di.contacts_list.DaggerContactsComponent;
 
 public class DaggerInjector implements Injector {
   private AppComponent appComponent;
